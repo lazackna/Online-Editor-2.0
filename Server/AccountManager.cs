dfc91d77baf06e8c8ed1ec8444afe14250494b27
jasper baijens <jasperbayens@outlook.com>
2021-10-27 00:03:30 +0200

    You can now login and create an account. Ping also added.

--- Server/AccountManager.cs
+++ Server/AccountManager.cs
@@ -11,23 +11,43 @@ namespace Server
 	{
 		public static string dataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Online_Editor");
 
-		public AccountManager() {
+		private const string PASSPATH = "Password.pass";
+
+		public AccountManager()
+		{
 
 		}
 
-		public static void InitializeServer() {
+		public static void InitializeServer()
+		{
 			if (!Directory.Exists(dataPath)) Directory.CreateDirectory(dataPath);
 		}
 
-		public bool CreateAccount (string name, string password) {
-		string path = Path.Combine(dataPath, name);
-			if (!Directory.Exists(path)) {
+		public bool CreateAccount(string name, string password)
+		{
+			string path = Path.Combine(dataPath, name);
+			if (!Directory.Exists(path))
+			{
 				Directory.CreateDirectory(path);
-				File.WriteAllText(Path.Combine(path, "Password.pass"), password);
+				File.WriteAllText(Path.Combine(path, PASSPATH), password);
 				return true;
 			}
 			return false;
 		}
+
+		public bool Login(string name, string password)
+		{
+			string root = Path.Combine(dataPath, name);
+			if (Directory.Exists(root) && File.Exists(Path.Combine(root, PASSPATH)))
+			{
+				string savedpass = File.ReadAllText(Path.Combine(root, PASSPATH));
+				if (password == savedpass) return true;
+			}
+
+			return false;
+		}
+
+
 
 	}
 }
